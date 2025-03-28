91b909aca5012cb8d795fbabc4dddfe459740640
rfflsy16 <rfflsy16@gmail.com>
2025-03-29 03:52:57 +0700

    feat: adding analytic vercel

--- my-portfolio/src/app/layout.tsx
+++ my-portfolio/src/app/layout.tsx
@@ -2,6 +2,7 @@ import Background from '@/components/Background';
 import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import { Analytics } from '@vercel/analytics/next';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Background />
         {children}
+        <Analytics/>
         </body>
     </html>
   );
