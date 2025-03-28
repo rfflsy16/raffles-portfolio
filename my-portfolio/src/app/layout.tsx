ebe07879f7060c67ff40acac7cd8ffdfccb83d75
rfflsy16 <rfflsy16@gmail.com>
2025-03-29 03:56:45 +0700

    feat: adding speed insights vercel

--- my-portfolio/src/app/layout.tsx
+++ my-portfolio/src/app/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -21,7 +22,8 @@ export default function RootLayout({
       <body className={inter.className}>
         <Background />
         {children}
-        <Analytics/>
+        <Analytics />
+        <SpeedInsights />
         </body>
     </html>
   );
