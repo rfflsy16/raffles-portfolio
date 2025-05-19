5084f4842739a634296cf8f9c845d0c4a8dc5c7b
rfflsy16 <rfflsy16@gmail.com>
2025-05-19 23:50:54 +0700

    fiz: tech stack on travelex

--- my-portfolio/src/constants/index.ts
+++ my-portfolio/src/constants/index.ts
@@ -146,7 +146,7 @@ export const projects = [
   {
     title: "Travelex",
     description: "A travel wishlist app with interactive maps integration, allowing users to save and plan their dream destinations.",
-    tech: ["React Native", "Expo", "TypeScript", "Google Maps API", "MongoDB", "Express.js", "JWT", "Context"],
+    tech: ["React", "Vite", "Javascript", "Google Maps API", "PostgreSQL", "Express.js", "JWT", "Redux"],
     date: "January 2025",
     image: "https://images.unsplash.com/photo-1469854523086-cc02fe5d8800?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2021&q=80",
     link: 'https://github.com/rfflsy16/travelex-individual-project',
